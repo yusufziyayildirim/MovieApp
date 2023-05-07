fc3c1d0e123efefc1dc8ae79f990551276dfaa8a
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-07 22:35:37 +0300

    HttpMethod enum

---
+++ MovieApp/Utilities/NetworkHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkHelpers.swift
+//  MovieApp
+//
+//  Created by Yusuf Ziya YILDIRIM on 7.05.2023.
+//
+
+import Foundation
+
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
