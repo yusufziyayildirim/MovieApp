72c480658c97960f606ced0d6cb68400bacc7bba
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-07 22:48:13 +0300

    APIURLs enum

--- MovieApp/Utilities/NetworkHelpers.swift
+++ MovieApp/Utilities/NetworkHelpers.swift
@@ -14,3 +14,19 @@ enum HttpMethod: String {
     case put = "PUT"
     case delete = "DELETE"
 }
+
+enum APIURLs {
+    static var apiKey = "e112ed72df8da5c3b38e4e6579896bc6&language"
+    
+    static func movies(page: Int) -> String {
+        "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)6&language=en-US&page=\(page)"
+    }
+    
+    static func imageURL(posterPath: String) -> String {
+        "https://image.tmdb.org/t/p/w500\(posterPath)"
+    }
+    
+    static func movieDetail(id: Int) -> String {
+        "https://api.themoviedb.org/3/movie/\(id)?api_key=\(apiKey)&language=en-US"
+    }
+}
