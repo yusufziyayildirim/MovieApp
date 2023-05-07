367926c43a090897c49cad4af1e362c5512315e0
Yusuf Ziya YILDIRIM <yusufziyayildirim@Yusuf-MacBook-Air.local>
2023-05-07 15:12:19 +0300

    initial-commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Yusuf Ziya YILDIRIM on 7.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
