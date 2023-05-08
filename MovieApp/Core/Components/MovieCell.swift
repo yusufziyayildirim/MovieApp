639f5b1c09e0efec1425a45d81b688412c847315
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-08 17:26:17 +0300

    Configure MovieCell

--- MovieApp/Core/Components/MovieCell.swift
+++ MovieApp/Core/Components/MovieCell.swift
@@ -9,20 +9,42 @@ import UIKit
 
 class MovieCell: UICollectionViewCell {
     static let reuseID = "MovieCell"
+    private var posterImageView: PosterImageView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         configureCell()
+        configurePosterImageView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        posterImageView.image = nil
+        posterImageView.cancelDownloading()
+    }
+    
     private func configureCell() {
-           backgroundColor = .systemGray5
-           layer.cornerRadius = 16
-           clipsToBounds = true
-       }
+        backgroundColor = .systemGray5
+        layer.cornerRadius = 16
+        clipsToBounds = true
+    }
+    
+    private func configurePosterImageView() {
+        posterImageView = PosterImageView(frame: .zero)
+        addSubview(posterImageView)
+        posterImageView.backgroundColor = .red
+
+        posterImageView.pinToEdgesOf(view: self)
+    }
+    
+    func setCell(movie: MovieResult) {
+        posterImageView.downloadImage(movie: movie)
+    }
+    
 }
