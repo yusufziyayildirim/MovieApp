2ad5564f1731be7c8e63201bec8aa6ea8916e061
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-08 17:23:52 +0300

    Update NetworkManager

--- MovieApp/Utilities/NetworkManager.swift
+++ MovieApp/Utilities/NetworkManager.swift
@@ -11,18 +11,21 @@ final class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
-    func request<T: Codable>(type: T.Type, url: String, method: HttpMethod, completion: @escaping(Result<T, Error>)->()) {
+    @discardableResult
+    func request<T: Codable>(type: T.Type, url: String, method: HttpMethod, completion: @escaping(Result<T, Error>)->()) -> URLSessionDataTask? {
         let session = URLSession.shared
+        var dataTask: URLSessionDataTask?
+        
         if let url = URL(string: url) {
-            
             var request = URLRequest(url: url)
             request.httpMethod = method.rawValue
             
-            let dataTask = session.dataTask(with: request) { [weak self] data, response, error in
+            dataTask = session.dataTask(with: request) { [weak self] data, response, error in
                 guard let self = self else { return }
                 
                 if let error = error {
                     completion(.failure(error))
+                    return
                 }
                 
                 guard
@@ -39,20 +42,27 @@ final class NetworkManager {
                 
                 self.handleResponse(data: data) { response in
                     completion(response)
-                    
                 }
             }
-            
-            dataTask.resume()
-        }
+            dataTask?.resume()
+       }
+        
+        return dataTask
     }
     
-    private func handleResponse<T: Codable>(data: Data, complete: @escaping(Result<T, Error>)->()) {
+    private func handleResponse<T: Codable>(data: Data, completion: @escaping(Result<T, Error>)->()) {
         do {
-            let result = try JSONDecoder().decode(T.self, from: data)
-            complete(.success(result))
+            /*
+             For the Data type, return the data as is without attempting to decode it since it may already be in the expected binary format
+             */
+            if T.self == Data.self {
+                completion(.success(data as! T))
+            }else{
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(result))
+            }
         } catch {
-            complete(.failure(error))
+            completion(.failure(error))
         }
     }
 }
