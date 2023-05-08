dad4afa1ea9b631162d54e72cbd621da6dc3e3ad
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-08 13:58:12 +0300

    CGFloat extension

---
+++ MovieApp/Extensions/CGFloat+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+Ext.swift
+//  MovieApp
+//
+//  Created by Yusuf Ziya YILDIRIM on 8.05.2023.
+//
+
+import UIKit
+
+extension CGFloat {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
