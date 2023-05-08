e49ed45e8b800300bba0e0f79ccf40f509bb96cc
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-05-08 17:24:09 +0300

    Update MovieService

--- MovieApp/Services/MovieService.swift
+++ MovieApp/Services/MovieService.swift
@@ -9,14 +9,14 @@ import Foundation
 
 class MovieService{
     
-    func getMovies(page: Int, completion: @escaping ((Movie?, Error?) -> ())){
+    func getMovies(page: Int, completion: @escaping ((Movie?) -> ())){
         
         NetworkManager.shared.request(type: Movie.self, url: APIURLs.movies(page: page), method: .get) { response in
             switch response {
             case .success(let data):
-                completion(data, nil)
+                completion(data)
             case .failure(let error):
-                completion(nil, error)
+                print(error)
             }
         }
     }
